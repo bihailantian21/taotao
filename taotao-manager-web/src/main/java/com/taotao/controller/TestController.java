5d1454ac61619e6243d6fe7de114773c88eee374
bihailantian21 <864620611@qq.com>
2019-08-18 23:48:28 +0800

    Initial commit

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.taotao.controller;
+
+import com.taotao.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 测试使用controller 查询当前的时间
+ */
+@Controller
+public class TestController {
+    @Autowired
+    private TestService testService;
+    /**
+     * 测试dubbo配置是否正常
+     * @return
+     */
+    @RequestMapping("/test/queryNow")
+    @ResponseBody
+    public String queryNow(){
+        System.out.println("wositestcontroller");
+
+        return testService.queryNow();
+    }
+}
+
+
---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/TestService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+public interface TestService {//测试接口
+    /**
+     * 从数据库中获取当前的时间
+     * @return
+     */
+    public String queryNow();//可以和上面的方法一样，也可以不一样
+}
+
